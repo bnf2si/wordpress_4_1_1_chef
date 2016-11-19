0e6d8c541388b8a38721e2c9ad49413e0140c906
Frank <Fra.Bernhardt@googlemail.com>
2016-11-19 18:30:36 +0100

    Cloudfront distribution added

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -16,8 +16,8 @@
 
 define('WP_REDIS_HOST', 'wordpress.r8uxg4.0001.euc1.cache.amazonaws.com');
  
-define('WP_HOME','http://wordpresselb-496185220.eu-central-1.elb.amazonaws.com');
-define('WP_SITEURL','http://wordpresselb-496185220.eu-central-1.elb.amazonaws.com');
+define('WP_HOME','http://dpbh6f7e6gl8y.cloudfront.net');
+define('WP_SITEURL','http://dpbh6f7e6gl8y.cloudfront.net');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
